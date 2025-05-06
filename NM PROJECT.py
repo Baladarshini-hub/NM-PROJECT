3af99df5f94612682e2a676a5b551ecf042d1877
Baladarshini-hub <sbaladarshini@gmail.com>
2025-05-06 09:50:30 +0530

    Add files via upload

---
+++ NM PROJECT.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the value of A"))
+b=int(input("Enter the value of B"))
+print("Before exchanging the values")
+("A=",a,"B=",b)
+temp=a
+a=b
+b=temp
+print("After exchanging the valuesof the variables")
+("A=",a,"B=",b)
